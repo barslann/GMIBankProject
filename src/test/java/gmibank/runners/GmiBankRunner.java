244167563ee4db526aa994c8c5c09b3b66b5ac6d
omegarug <g.yalcin33@hotmail.com>
2021-06-08 00:42:03 -0500

    first commit

--- src/test/java/gmibank/runners/GmiBankRunner.java
+++ src/test/java/gmibank/runners/GmiBankRunner.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
+        monochrome = true,
         tags = "@adminSignIn"
 )
 public class GmiBankRunner {
